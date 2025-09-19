ed8e24ef396b11009611a2fb387afce3a2b6a2c7
Jorge Lorenzo <jorgeluismtz.11@gmail.com>
2025-09-19 09:39:43 -0600

    Implement API Rest and logs

--- automation/amazon_flow.py
+++ automation/amazon_flow.py
@@ -1,1 +1,17 @@
 # Amazon flow to buy a TV
+from playwright.async_api import async_playwright
+import logging
+
+AMAZON_URL = "https://www.amazon.com.mx/"
+
+async def run_amazon_flow(email: str, password: str, mode: bool):
+    logging.info("Iniciando flujo de Amazon")
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
+        
+        await page.goto(AMAZON_URL)
+        logging.info("Finaliza flujo")
+        
+        return "Flujo completado"
--- tests/test_amazon_flow.py
+++ tests/test_amazon_flow.py
@@ -1,1 +1,18 @@
 # perform API testing
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+
+payload = {
+        "email": os.getenv("AMAZON_EMAIL"),
+        "password": os.getenv("AMAZON_PASSWORD"),
+        "headless": False
+    }
+
+response = requests.post(API_URL, json=payload)
+
+print(f"{response.status_code}")
