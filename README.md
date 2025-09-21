1af7972df83467486e0d126c562f1b5551a3a04d
Jorge Lorenzo <jorgeluismtz.11@gmail.com>
2025-09-21 13:00:11 -0600

    Add readme and minor fixes to the code

---
+++ README.md
@@ -0,0 +1,67 @@
+# Amazon Purchase Flow Automation with Playwright
+
+This project simulates a purchase flow on Amazon by using Playwright with Python. The flow includes:
+- Amazon login
+- Search for the first TV result
+- Add to cart
+- Checkout simulation
+
+This project uses a API Rest with FastAPI to execute the flow, the data input are:
+- Email
+- Password
+- Headless mode (True or False)
+
+## Technology
+
+- [Python ≥ 3.8](https://www.python.org/)
+- [Playwright](https://playwright.dev/python/)
+- [FastAPI](https://fastapi.tiangolo.com/)
+- [Uvicorn](https://www.uvicorn.org/)
+- [python-dotenv](https://pypi.org/project/python-dotenv/)
+- [requests](https://pypi.org/project/requests/)
+- [pytest-playwright](https://pypi.org/project/pytest-playwright/)
+
+## Initial Setup
+
+Initial project setup, configs and installation of dependencies:
+
+#### 1. Clone this repository
+```bash
+git clone https://github.com/tu-usuario/amazon-playwright-automation.git
+cd amazon-playwright-automation
+```
+#### 2. Create and activate a virtual enviroment
+```bash
+python -m venv venv
+venv\Scripts\activate
+```
+#### 3. Install dependencies or libraries
+```bash
+pip install -r requirements.txt
+```
+#### 4. Install Playwright drivers
+```bash
+playwright install
+```
+#### 5. Copy the .env.example to a .env and add your Amazon credentials
+
+## Deployment
+
+To deploy this project run:
+
+#### 1. Start the API
+```bash
+uvicorn app.main:app
+```
+#### 2. Execute pytest file (test_amazon_flow.py) with:
+```bash
+pytest -v
+```
+
+## Notes
+
+This project doesn't perform actual purchases, it only simulates the flow.
+
+## Author
+
+Project developed by [@JorgeLore](https://github.com/JorgeLore)
--- automation/amazon_bot.py
+++ automation/amazon_bot.py
@@ -1,21 +1,33 @@
 # Amazon bot to navigate from login to checkout
 from automation.utils import click_if_visible, wait_for_button_and_clic
 from playwright.async_api import async_playwright
+import logging
 
 AMAZON_URL = "https://www.amazon.com.mx/"
 
 class AmazonBot:
+    """
+    This class encapsulates browser setup, navigation, login, and interaction logic
+    for simulating a purchase on Amazon. 
+    Attributes:
+        email (str): Amazon account email.
+        password (str): Amazon account password.
+        mode (bool): Execution mode. True for headless, False for headed.
+    """
+
     def __init__(self, email: str, password: str, mode: bool):
         self.email = email
         self.password = password
         self.mode = mode
         self.page = None
         self.browser = None
+        self.context = None
     
     async def __aenter__(self):
         self.playwright = await async_playwright().start()
         self.browser = await self.playwright.chromium.launch(headless=self.mode)
-        self.page = await self.browser.new_page()
+        self.context = await self.browser.new_context(viewport={"width": 1920, "height": 1080})
+        self.page = await self.context.new_page()
         return self
     
     async def __aexit__(self, exc_type, exc, tb):
@@ -32,7 +44,7 @@ class AmazonBot:
         await self.page.get_by_role("button", name="Continuar").click()
         await self.page.get_by_role("textbox", name="Contraseña").fill(self.password)
         await self.page.get_by_role("button", name="Iniciar sesión").click()
-    
+
     async def search_amazon(self):
     # 2. Search TV
         await wait_for_button_and_clic(self.page, "Abrir el menú de Todas las")
@@ -43,12 +55,13 @@ class AmazonBot:
     
     async def add_to_cart_amazon(self):
     # 3. Select first result and add to cart
-        await click_if_visible(self.page, "Ver opciones")
-        await wait_for_button_and_clic(self.page, "Agregar al Carrito")
+        await self.page.locator(".a-size-base-plus").first.click()
+        await self.page.get_by_role("button", name="Agregar al Carrito").click()
         await click_if_visible(self.page, "No, gracias")
         await self.page.evaluate("window.scrollTo(0, 0)")
         await self.page.wait_for_timeout(500)
         await self.page.get_by_role("link", name="artículo en el carrito").click()
+        logging.info("Product added to the cart")
         
     async def checkout_amazon(self):
         # 4. Checkout
@@ -58,9 +71,14 @@ class AmazonBot:
             checkout_locator = self.page.locator("#nav-checkout-title-header-text")
             await checkout_locator.wait_for(state="visible")
             checkout_text = await checkout_locator.inner_text()
-        except:
-            checkout_text = "Error reaching the payment page "
+            await self.page.wait_for_timeout(1000)
+            await self.page.screenshot(path=f"./logs/test_result_{self.mode}_headless_mode.png")
+        except Exception as e:
+            logging.error("Error accessing the payment page")
+            checkout_text = "Error accessing the payment page"
+            raise
         # 5. Postcondition: eliminate the TV added to test this flow multiple times
+        logging.info("Access to payment page successfully")
         await self.page.get_by_label("Volver al carrito").click()
         await self.page.get_by_text("Eliminar").click()
         
